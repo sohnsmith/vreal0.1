9d1f887be35b4861dc45af6092b065bd2bce47dc
Versy <sohnsmith@sonjaehyeong-ui-MacBookPro.local>
2023-04-25 21:56:33 +0900

    fix 0425_2156

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,15 +7,44 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('vreal'),
+        title: const Text('vreal'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(onPressed: () {}, child: Text('블랙마카')),
-            ElevatedButton(onPressed: () {}, child: Text('아르기닌')),
-            ElevatedButton(onPressed: () {}, child: Text('쏘팔메토')),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 40),
+                child: Text(
+                  '블랙마카',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 40),
+                child: Text(
+                  '아르기닌',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 40),
+                child: Text(
+                  '쏘팔메토',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
           ],
         ),
       ),
